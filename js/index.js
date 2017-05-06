b4b1fb922e50e237e9530043c3b03574f3a01410
大平台 <dktseng2005@gmail.com>
2017-05-06 13:34:41 +0800

    abc

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.lig($("#aaa"))
+
+function hello() {
+    var aaa = $("#aaa").val();
+    var ddd = $("#aaa").val();
+    alert(你的名子 + # aaa + 你的年齡 + # ddd)
+}
+ if( $("#one-checkbox").prop("checked") ){ // 回傳布林值
+            alert( "你是"+$("#one-checkbox").val() );
+$("#ok").on("#click", hello);
