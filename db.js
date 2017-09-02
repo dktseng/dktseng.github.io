a5009903b729b3cd404840e1d1f452f7a6be4aa1
大平台 <dktseng2005@gmail.com>
2017-09-02 14:18:25 +0800

    Add files via upload

--- db.js
+++ db.js
@@ -1,4 +1,22 @@
 var fdb = new ForerunnerDB();
-var db = fdb.db("記帳表");
-var studentCollection = db.collection('students');
-accountingcllection
+var db = fdb.db("accounting");
+var accountingCollection = db.collection('ggqqtat');
+accountingCollection.load();
+
+function createAccountingHTMLString(date, category, item, cost){
+	return "<tr><td>"+date+"</td><td>"+category+"</td><td>"+item+"</td><td>"+cost+"</td></tr>"
+}
+
+setTimeout(function(){
+	var accountings = accountingCollection.find(
+		{},
+		{
+			$orderBy: {"date":-1},
+			$limit: 10
+		}
+	);
+	for (var i = 0; i < accountings.length; i++) {
+		$("#accountingTable").append(createAccountingHTMLString(accountings[i].date, accountings[i].category, accountings[i].item, accountings[i].cost))
+	}
+
+}, 500);
